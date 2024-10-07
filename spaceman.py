6db8057db985bd53ca4ca0a711b4fc58015839c2
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-07 18:56:32 -0400

    completed is_word_guessed function

--- spaceman.py
+++ spaceman.py
@@ -1,13 +1,6 @@
 import random
 
 def load_word():
-    '''
-    A function that reads a text file of words and randomly selects one to use as the secret word
-        from the list.
-
-    Returns: 
-           string: The secret word to be used in the spaceman guessing game
-    '''
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close()
@@ -17,18 +10,10 @@ def load_word():
     return secret_word
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
-
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns: 
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+    for letter in secret_word:
+        if letter not in letters_guessed:
+            return False
+        return True
 
 def get_guessed_word(secret_word, letters_guessed):
     '''
