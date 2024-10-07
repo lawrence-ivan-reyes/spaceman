107f9ade42c9e591b1d9bf1eefc4e03814f97b23
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-07 19:01:57 -0400

    completed get_guessed_word function

--- spaceman.py
+++ spaceman.py
@@ -16,21 +16,13 @@ def is_word_guessed(secret_word, letters_guessed):
         return True
 
 def get_guessed_word(secret_word, letters_guessed):
-    '''
-    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
-
-    Args: 
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns: 
-        string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
-    pass
-
+    guessed_word = ""
+    for letter in secret_word:
+        if letter in letters_guessed:
+            guessed_word += letter
+        else:
+            guessed_word += "_"
+        return guessed_word
 
 def is_guess_in_word(guess, secret_word):
     '''
