7f48e0763a3b1a81c189199e14785c8c74103a96
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-08 18:05:02 -0400

    stretch challenge: asking user if they want to play the game again at the end

--- spaceman.py
+++ spaceman.py
@@ -32,41 +32,48 @@ def is_guess_in_word(guess, secret_word):
 
 # main game function
 def spaceman(secret_word):
-    print("Welcome to Spaceman!")
-    print("To play, guess the secret word one letter at a time.\nYou have 7 chances, and for each incorrect guess, a part of the spaceman will be drawn.")
-    print(f"\nThe secret word has {len(secret_word)} letters. Good luck and have fun!\n")
+    while True: # stretch challenge: loop to keep playing if the user chooses 'yes' to playing again at the end
+        print("Welcome to Spaceman!")
+        print("To play, guess the secret word one letter at a time.\nYou have 7 chances, and for each incorrect guess, a part of the spaceman will be drawn.")
+        print(f"\nThe secret word has {len(secret_word)} letters. Good luck and have fun!\n")
 
-    letters_guessed = [] # initializing a list in order to store the letters that have been guessed
-    guesses_left = 7 # player starts with 7 guesses
+        letters_guessed = [] # initializing a list in order to store the letters that have been guessed
+        guesses_left = 7 # player starts with 7 guesses
 
-    # this loop will continue until guesses run out OR until the word has been correctly guessed 
-    while guesses_left > 0:
-        print("Current guess: " + get_guessed_word(secret_word, letters_guessed))
-        print(f"You have {guesses_left} guesses left!")
+        # this loop will continue until guesses run out OR until the word has been correctly guessed 
+        while guesses_left > 0:
+            print("Current guess: " + get_guessed_word(secret_word, letters_guessed))
+            print(f"You have {guesses_left} guesses left!")
 
-        guess = input("\nPlease guess one letter: ").lower()
+            guess = input("\nPlease guess one letter: ").lower()
 
-        if len(guess) != 1 or not guess.isalpha(): # stretch challenge: this is ensuring a valid input of a SINGLE alphabetic character 
-            print("\nInvalid. Please guess a single letter!")
-            continue # stretch challenge: goes back to beginning of loop to allow user to enter another guess without reducing guess count
+            if len(guess) != 1 or not guess.isalpha(): # stretch challenge: this is ensuring a valid input of a SINGLE alphabetic character 
+                print("\nInvalid. Please guess a single letter!")
+                continue # stretch challenge: goes back to beginning of loop to allow user to enter another guess without reducing guess count
 
-        if guess in letters_guessed:
-            print("\nUh-oh. You already guessed that letter!")
-            continue # will skip the rest of the loop and ask for another letter
+            if guess in letters_guessed:
+                print("\nUh-oh. You already guessed that letter!")
+                continue # will skip the rest of the loop and ask for another letter
 
-        letters_guessed.append(guess)
+            letters_guessed.append(guess)
 
-        if is_guess_in_word(guess, secret_word):
-            print("\nGood guess.")
+            if is_guess_in_word(guess, secret_word):
+                print("\nGood guess.")
+            else:
+                print("\nIncorrect guess.")
+                guesses_left -= 1
+
+            if is_word_guessed(secret_word, letters_guessed):
+                print("\nYay! You guessed the secret word: " + secret_word)
+                break # game ends here if the word has been guessed
         else:
-            print("\nIncorrect guess.")
-            guesses_left -= 1
+            print("\nSorry, you lost. The secret word was: " + secret_word) # only happens if the player runs out of guesses
 
-        if is_word_guessed(secret_word, letters_guessed):
-            print("\nYay! You guessed the secret word: " + secret_word)
-            break # game ends here if the word has been guessed
-    else:
-        print("\nSorry, you lost. The secret word was: " + secret_word) # only happens if the player runs out of guesses
+        # stretch challenge: asking user if they want to play again after the game ends
+        play_again = input("\nThanks for playing. Would you like to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing! See you next time.")
+            break
 
-secret_word = load_word()
-spaceman(secret_word)
+# start game
+spaceman()
