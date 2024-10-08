7488f47a43f696a3148f801fbe01b08d7ae6baf6
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-08 18:58:23 -0400

    revised comments and output language

--- spaceman.py
+++ spaceman.py
@@ -10,24 +10,21 @@ def load_word():
     secret_word = random.choice(words_list)
     return secret_word
 
-# function to check if the word has been FULLY guessed
 def is_word_guessed(secret_word, letters_guessed):
     for letter in secret_word:
         if letter not in letters_guessed: 
             return False
     return True 
 
-# function to display the CURRENT state of the guessed word
 def get_guessed_word(secret_word, letters_guessed):
     guessed_word = ""
-    for letter in secret_word: # for every letter in the secret word, check if it's been guessed
+    for letter in secret_word: 
         if letter in letters_guessed:
             guessed_word += letter 
         else:
             guessed_word += "_" 
     return guessed_word
 
-# function to check if the guesser letter is in the secret word
 def is_guess_in_word(guess, secret_word):
     return guess in secret_word
 
@@ -39,7 +36,7 @@ def spaceman():
         print("To play, guess the secret word one letter at a time.\nFor each incorrect guess, a part of the spaceman will be drawn.")
         print(f"\nThe secret word has {len(secret_word)} letters. Good luck and have fun!\n")
 
-        letters_guessed = [] # initializing a list in order to store the letters that have been guessed
+        letters_guessed = []
         incorrect_guesses = -0 # stretch challenge: initializing this new variable to 0 to satisfy the spaceman ascii art
         max_incorrect_guesses = len(secret_word) # stretch challenge: initializing this variable to help with the ascii art challenge
 
@@ -57,7 +54,7 @@ def spaceman():
 
             if guess in letters_guessed:
                 print("\nUh-oh. You already guessed that letter!")
-                continue # will skip the rest of the loop and ask for another letter
+                continue 
 
             letters_guessed.append(guess)
 
@@ -69,13 +66,13 @@ def spaceman():
 
             if is_word_guessed(secret_word, letters_guessed):
                 print("\nYay! You guessed the secret word: " + secret_word) 
-                break # game ends here if the word has been guessed
+                break
         else:
             draw_spaceman(incorrect_guesses) # stretch challenge: draw spaceman when user loses game
             print("\nSorry, you lost. The secret word was: " + secret_word) # stretch challenge: shows user secret word when they lose
 
         # stretch challenge: asking user if they want to play again after the game ends
-        play_again = input("\nThanks for playing. Would you like to play again? (yes/no): ").lower()
+        play_again = input("\nWould you like to play again? (yes/no): ").lower()
         if play_again != 'yes':
             print("Thanks for playing! See you next time.")
             break
